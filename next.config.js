9ae08e227c14068e08d6b5e4844a0e7fac1deb03
ErMaheshChand <mahesh.khatnawal@gmail.com>
2026-09-12 23:30:18 +0530

    Integrate PWA support into Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+const withPWA = require('next-pwa').default({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = withPWA(nextConfig);
